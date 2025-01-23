538d4ccbcdc7761a96c88f8f72c8a3608d8554b4
Malcolm-XY <m18307300600@gmail.com>
2025-01-24 02:22:00 +0900

    20250124

--- Models/models.py
+++ Models/models.py
@@ -9,27 +9,101 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class CNN2DModel_3layers_adaptive_avgpool(nn.Module):
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN2DModel_3layers_adaptive_avgpool, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        # 第三层卷积 + BatchNorm + 池化
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.AdaptiveAvgPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=256, out_features=128)
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
+
+class CNN2DModel_3layers_adaptive_maxpool(nn.Module):
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN2DModel_3layers_adaptive_maxpool, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第三层卷积 + BatchNorm + 池化
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=256, out_features=128)
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
+
 class CNN2DModel_3layers_avgpool(nn.Module):
     def __init__(self, channels=3, num_classes=3):
         super(CNN2DModel_3layers_avgpool, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.AvgPool2d(kernel_size=4, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.AvgPool2d(kernel_size=3, stride=3)
 
         # 第二层卷积 + BatchNorm + 池化
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.AvgPool2d(kernel_size=4, stride=4)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=3)
 
         # 第三层卷积 + BatchNorm + 池化
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.AvgPool2d(kernel_size=4, stride=4)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.AvgPool2d(kernel_size=3, stride=3)
 
         # 全连接层
-        self.fc1 = nn.Linear(in_features=256, out_features=128)
+        self.fc1 = nn.Linear(in_features=3*3*128, out_features=128)
         self.dropout1 = nn.Dropout(p=0.25)
         self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
 
@@ -52,21 +126,21 @@ class CNN2DModel_3layers_maxpool(nn.Module):
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d(kernel_size=4, stride=4)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 第二层卷积 + BatchNorm + 池化
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d(kernel_size=4, stride=4)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 第三层卷积 + BatchNorm + 池化
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d(kernel_size=4, stride=4)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 全连接层
-        self.fc1 = nn.Linear(in_features=256, out_features=128)
+        self.fc1 = nn.Linear(in_features=3*3*128, out_features=128)
         self.dropout1 = nn.Dropout(p=0.25)
         self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
 
@@ -90,12 +164,12 @@ class CNN2DModel_4layers_avgpool(nn.Module):
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.pool1 = nn.AvgPool2d(kernel_size=3, stride=3)
 
         # 第二层卷积 + BatchNorm + 池化
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=3)
 
         # 第三层卷积 + BatchNorm + 池化
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
@@ -105,7 +179,7 @@ class CNN2DModel_4layers_avgpool(nn.Module):
         # 第四层卷积 + BatchNorm + 池化
         self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm2d(512)
-        self.pool4 = nn.AvgPool2d(kernel_size=5, stride=5)
+        self.pool4 = nn.AvgPool2d(kernel_size=3, stride=3)
 
         # 全连接层
         self.fc1 = nn.Linear(in_features=512, out_features=256)
@@ -138,12 +212,12 @@ class CNN2DModel_4layers_maxpool(nn.Module):
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 第二层卷积 + BatchNorm + 池化
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 第三层卷积 + BatchNorm + 池化
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
@@ -153,7 +227,7 @@ class CNN2DModel_4layers_maxpool(nn.Module):
         # 第四层卷积 + BatchNorm + 池化
         self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm2d(512)
-        self.pool4 = nn.MaxPool2d(kernel_size=5, stride=5)
+        self.pool4 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 全连接层
         self.fc1 = nn.Linear(in_features=512, out_features=256)
