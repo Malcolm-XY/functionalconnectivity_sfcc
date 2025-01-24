36e41fa8a366d90bd3a5983a657d54f197067f8f
Malcolm-XY <m18307300600@gmail.com>
2025-01-25 05:03:27 +0900

    20250125

--- Models/models.py
+++ Models/models.py
@@ -9,24 +9,143 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CNN2DModel_3layers_adaptive_avgpool(nn.Module):
+# %% adaptive
+class CNN_2layers_adaptive_avgpool_3(nn.Module):
+    """
+    this model is identified as:
+    2 convolution layers: c1 = c2 = 3, 1
+    1 avgpool layers: p1 = 3, 3
+    1 global maxpool
+    """
     def __init__(self, channels=3, num_classes=3):
-        super(CNN2DModel_3layers_adaptive_avgpool, self).__init__()
+        super(CNN_2layers_adaptive_avgpool_3, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.AvgPool2d(kernel_size=3, stride=3)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=64, out_features=32)
+        self.fc2 = nn.Linear(in_features=32, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class CNN_2layers_adaptive_maxpool_3(nn.Module):
+    """
+    this model is identified as:
+    2 convolution layers: c1 = c2 = 3, 1
+    1 maxpool layers: p1 = 3, 3
+    1 global maxpool
+    """
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN_2layers_adaptive_maxpool_3, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=3)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=64, out_features=32)
+        self.fc2 = nn.Linear(in_features=32, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class CNN_3layers_adaptive_avgpool_3(nn.Module):
+    """
+    this model is identified as:
+    3 convolution layers: c1 = c2 = c3 = 3, 1
+    2 avgpool layers: p1 = p2 = 3, 3
+    1 global maxpool
+    """
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN_3layers_adaptive_avgpool_3, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.AvgPool2d(kernel_size=3, stride=3)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=3)
+
+        # 第三层卷积 + BatchNorm + 池化
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=128, out_features=64)
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(in_features=64, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
+
+class CNN_3layers_adaptive_maxpool_3(nn.Module):
+    """
+    this model is identified as:
+    3 convolution layers: c1 = c2 = c3 = 3, 1
+    2 maxpool layers: p1 = p2 = 3, 3
+    1 global maxpool
+    """
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN_3layers_adaptive_maxpool_3, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 第二层卷积 + BatchNorm + 池化
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=3)
 
         # 第三层卷积 + BatchNorm + 池化
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.AdaptiveAvgPool2d((1, 1)) 
+        self.pool3 = nn.AdaptiveMaxPool2d((1, 1)) 
 
         # 全连接层
         self.fc1 = nn.Linear(in_features=256, out_features=128)
@@ -46,9 +165,59 @@ class CNN2DModel_3layers_adaptive_avgpool(nn.Module):
         x = self.fc2(x)
         return x
 
-class CNN2DModel_3layers_adaptive_maxpool(nn.Module):
+
+class CNN_3layers_adaptive_avgpool_2(nn.Module):
+    """
+    this model is identified as:
+    3 convolution layers: c1 = c2 = c3 = 3, 1
+    2 avgpool layers: p1 = p2 = 2, 2
+    1 global maxpool
+    """
     def __init__(self, channels=3, num_classes=3):
-        super(CNN2DModel_3layers_adaptive_maxpool, self).__init__()
+        super(CNN_3layers_adaptive_avgpool_2, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        # 第三层卷积 + BatchNorm + 池化
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.pool3 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=256, out_features=128)
+        self.dropout1 = nn.Dropout(p=0.25)
+        self.fc2 = nn.Linear(in_features=128, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool3(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
+
+class CNN_3layers_adaptive_maxpool_2(nn.Module):
+    """
+    this model is identified as:
+    3 convolution layers: c1 = c2 = c3 = 3, 1
+    2 maxpool layers: p1 = p2 = 2, 2
+    1 global maxpool
+    """
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN_3layers_adaptive_maxpool_2, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
@@ -83,9 +252,15 @@ class CNN2DModel_3layers_adaptive_maxpool(nn.Module):
         x = self.fc2(x)
         return x
 
-class CNN2DModel_3layers_avgpool(nn.Module):
+# %% fixed
+class CNN_3layers_avgpool(nn.Module):
+    """
+    this model is identified as:
+    3 convolution layers: c1 = c2 = c3 = 3, 1
+    3 avgpool layers: p1 = p2 = p3 = 3, 3
+    """
     def __init__(self, channels=3, num_classes=3):
-        super(CNN2DModel_3layers_avgpool, self).__init__()
+        super(CNN_3layers_avgpool, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
@@ -120,9 +295,14 @@ class CNN2DModel_3layers_avgpool(nn.Module):
         x = self.fc2(x)
         return x
 
-class CNN2DModel_3layers_maxpool(nn.Module):
+class CNN_3layers_maxpool(nn.Module):
+    """
+    this model is identified as:
+    3 convolution layers: c1 = c2 = c3 = 3, 1
+    3 maxpool layers: p1 = p2 = p3 = 3, 3
+    """
     def __init__(self, channels=3, num_classes=3):
-        super(CNN2DModel_3layers_maxpool, self).__init__()
+        super(CNN_3layers_maxpool, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
@@ -157,9 +337,10 @@ class CNN2DModel_3layers_maxpool(nn.Module):
         x = self.fc2(x)
         return x
 
-class CNN2DModel_4layers_avgpool(nn.Module):
+# %% 4 layers
+class CNN_4layers_avgpool(nn.Module):
     def __init__(self, channels=3, num_classes=3):
-        super(CNN2DModel_4layers_avgpool, self).__init__()
+        super(CNN_4layers_avgpool, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
@@ -205,9 +386,9 @@ class CNN2DModel_4layers_avgpool(nn.Module):
         x = self.fc3(x)
         return x
     
-class CNN2DModel_4layers_maxpool(nn.Module):
+class CNN_4layers_maxpool(nn.Module):
     def __init__(self, channels=3, num_classes=3):
-        super(CNN2DModel_4layers_maxpool, self).__init__()
+        super(CNN_4layers_maxpool, self).__init__()
 
         # 第一层卷积 + BatchNorm + 池化
         self.conv1 = nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=3, stride=1, padding=1)
--- cnn_val_circle.py
+++ cnn_val_circle.py
@@ -198,10 +198,10 @@ def end_program_actions(play_sound=True, shutdown=False, countdown_seconds=120):
 # %% Usage; training settings
 from models import models #, models_multiscale
 
-model = models.CNN2DModel_3layers_maxpool()
+model = models.CNN_2layers_adaptive_avgpool_3()
 
 # %% validation 1
-fcnetwork, feature, subject_range, experiment_range = 'sfcc', 'PLV', range(1, 16), range(1, 4)
+fcnetwork, feature, subject_range, experiment_range = 'sfcc', 'PCC', range(6, 16), range(1, 4)
 
 # trainning and validation
 results = cnn_cross_validation_circle(model, fcnetwork, feature, subject_range, experiment_range)
@@ -212,7 +212,7 @@ filename = f"{fcnetwork}_{type(model).__name__}_{feature}.xlsx"
 save_results_to_xlsx_append(results, output_dir, filename)
 
 # %% validation 2
-fcnetwork, feature, subject_range, experiment_range = 'cm', 'PLV', range(1, 16), range(1, 4)
+fcnetwork, feature, subject_range, experiment_range = 'cm', 'PCC', range(6, 16), range(1, 4)
 
 # trainning and validation
 results = cnn_cross_validation_circle(model, fcnetwork, feature, subject_range, experiment_range)
@@ -223,15 +223,15 @@ filename = f"{fcnetwork}_{type(model).__name__}_{feature}.xlsx"
 save_results_to_xlsx_append(results, output_dir, filename)
 
 # %% validation 2
-fcnetwork, feature, subject_range, experiment_range = 'vc', 'PLV', range(1, 16), range(1, 4)
+# fcnetwork, feature, subject_range, experiment_range = 'vc', 'PLV', range(1, 16), range(1, 4)
 
-# trainning and validation
-results = cnn_cross_validation_circle(model, fcnetwork, feature, subject_range, experiment_range)
+# # trainning and validation
+# results = cnn_cross_validation_circle(model, fcnetwork, feature, subject_range, experiment_range)
 
-# Save results to XLSX (append mode)
-output_dir = os.path.join(os.getcwd(), 'results')
-filename = f"{fcnetwork}_{type(model).__name__}_{feature}.xlsx"
-save_results_to_xlsx_append(results, output_dir, filename)
+# # Save results to XLSX (append mode)
+# output_dir = os.path.join(os.getcwd(), 'results')
+# filename = f"{fcnetwork}_{type(model).__name__}_{feature}.xlsx"
+# save_results_to_xlsx_append(results, output_dir, filename)
 
 # %% End program actions
 end_program_actions(play_sound=True, shutdown=True)
