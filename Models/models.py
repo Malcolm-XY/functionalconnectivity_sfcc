b219c8432d4fd45d5491e0057ace250a4d133494
Malcolm-XY <m18307300600@gmail.com>
2025-01-25 21:19:40 +0900

    20250125

--- Models/models.py
+++ Models/models.py
@@ -10,6 +10,75 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # %% adaptive
+class CNN_2layers_adaptive_avgpool_2(nn.Module):
+    """
+    this model is identified as:
+    2 convolution layers: c1 = c2 = 3, 1
+    1 avgpool layers: p1 = 2, 2
+    1 global maxpool
+    """
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN_2layers_adaptive_avgpool_2, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=64, out_features=32)
+        self.fc2 = nn.Linear(in_features=32, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class CNN_2layers_adaptive_maxpool_2(nn.Module):
+    """
+    this model is identified as:
+    2 convolution layers: c1 = c2 = 3, 1
+    1 maxpool layers: p1 = 2, 2
+    1 global maxpool
+    """
+    def __init__(self, channels=3, num_classes=3):
+        super(CNN_2layers_adaptive_maxpool_2, self).__init__()
+
+        # 第一层卷积 + BatchNorm + 池化
+        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 第二层卷积 + BatchNorm + 池化
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AdaptiveMaxPool2d((1, 1)) 
+
+        # 全连接层
+        self.fc1 = nn.Linear(in_features=64, out_features=32)
+        self.fc2 = nn.Linear(in_features=32, out_features=num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool1(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)  # 展平层
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
 class CNN_2layers_adaptive_avgpool_3(nn.Module):
     """
     this model is identified as:
